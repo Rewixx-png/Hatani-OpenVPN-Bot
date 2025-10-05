6f521b2bfab4ec3f332d254c28765efc382641ca
Rewixx-png <kustarnikovseryj@gmail.com>
2025-10-05 18:01:03 +0300

    Add config.example.py and ignore config.py

---
+++ config.example.py
@@ -0,0 +1,20 @@
+# --- Основные настройки ---
+BOT_TOKEN = "ВАШ_API_ТОКЕН" 
+
+# --- Настройки доступа ---
+ALLOWED_CHAT_ID = -1002033901364
+
+# --- Пути и URL ---
+VPN_SCRIPT_PATH = "/root/Bots/HataniVPN/CreateVPN/vpn_manager.sh"
+CLIENT_CONFIGS_DIR = "/root/" 
+
+# URL-адреса клиентов OpenVPN
+OPENVPN_APK_URL = "https://github.com/schwabe/ics-openvpn/releases/download/v0.7.61/ics-openvpn-0.7.61.apk"
+OPENVPN_MSI_URL = "https://openvpn.net/downloads/openvpn-connect-v3-windows.msi"
+OPENVPN_IOS_URL = "https://itunes.apple.com/us/app/openvpn-connect/id590379981?mt=8"
+
+# Временная папка для скачивания файлов
+TEMP_DOWNLOAD_DIR = "/tmp/"
+
+# --- Настройки базы данных ---
+DB_NAME = "vpn_users.db"
